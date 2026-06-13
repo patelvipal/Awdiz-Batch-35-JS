078b73d4697bb3712539be0d58a0908e1806d8bb
Patel Vipal <vipal4460@gmail.com>
2026-06-13 23:56:27 +0530

    Added new assignment

---
+++ Dat-4/index.js
@@ -0,0 +1,84 @@
+//1. Array
+
+var players = [
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+  "Gill",
+  "Rahul",
+  "Vipal",
+];
+console.log(players);
+console.log(players[1]);
+console.log(players[2]);
+console.log(players[3]);
+console.log(players.length);
+console.log(players[players.length - 1]);
+
+console.log((players.length - 1) / 2);
+console.log(players[(players.length - 1) / 2]);
+players[(players.length - 1) / 2] = "Vipal";
+console.log(players[(players.length - 1) / 2]);
+
+players[90] = "Hello Guys";
+console.log(players[62]);
+console.log(players);
+console.log(players[30]);
